710dd9f3a9e5a03ac10b6fdd5e69bbffaf7170f6
papschulo <ockyyudah7@gmail.com>
2026-01-04 00:14:35 +0700

    MENYELESAIKAN COMPANY PROFILE FRAME WORK

---
+++ js/gsap.js
@@ -0,0 +1,4 @@
+gsap.from('header', {duration:1.5, y:-100, ease:"power2"})
+// gsap.from('.container-home h1', {duration:1, delay:0.3 ,x:'-100%', opacity:0, ease:"power3"})
+// gsap.from('.container-home p', {duration:1.2, delay:0.5 ,x:'100%', opacity:0, ease:"power3"})
+gsap.from('.container-home', {duration:1.8, x:'-100%', opacity:0, ease:'exposcale'})
